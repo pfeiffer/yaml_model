44841922de03d465cd0b7aa7c5d7576a95b0c286
Michael Bleigh <michael@intridea.com>
2010-04-13 14:55:08 -0400

    Possibly fixing another Rails problem.

--- lib/yaml_model/rails.rb
+++ lib/yaml_model/rails.rb
@@ -3,7 +3,8 @@ require 'yaml_model'
 class YamlModel
   class Rails < ::YamlModel
     def self.yaml_file
-      @yaml_file ||= ::Rails.root + "/config/#{name.to_s.underscore.pluralize}.yml"
+      @yaml_file ||= ::Rails.root.to_s + "/config/#{name.to_s.underscore.pluralize}.yml"
+      super
     end
   end
 end
